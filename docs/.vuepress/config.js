3ed9b42da8a184641537b4510218a4c20ed5be1f
yangmaoting <524250484@qq.com>
2023-08-31 10:52:26 +0800

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: '安卓深入浅出',
+    description: 'Just playing Android',
+    themeConfig:{
+        nav:[
+            {
+                text:"首页",link:"/",
+            },
+            {
+                text: "晴天小庭",
+                items: [
+                    { text: "掘金", link: "https://juejin.cn/user/114798491603527" },
+                ]
+            }
+        ]
+    }
+}
